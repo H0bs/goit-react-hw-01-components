e1263401eeb7a8a0bf2e57e4b90061a066d65afb
Oleksandr Veremchuk <verem4uk@hotmail.com>
2022-10-08 14:19:11 +0300

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './Friends/FriendList';
-import { TransactionHistory } from './Transactions/TransactionHistory';
+import { Profile } from './components/Profile/Profile';
+import { Statistics } from './components/Statistics/Statistics';
+import { FriendList } from './components/Friends/FriendList';
+import { TransactionHistory } from './components/Transactions/TransactionHistory';
 import { Container } from './App.styled';
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
